4de77c3de0fa1cb6d25cb95e76eeefb924950380
ShikiSuen <shikisuen@outlook.com>
2024-08-05 09:26:53 +0800

    Attempt to fix compilation issue on Linux.

--- Sources/EnkaDBGeneratorModule/EDBG_PublicHeaderImpl.swift
+++ Sources/EnkaDBGeneratorModule/EDBG_PublicHeaderImpl.swift
@@ -3,6 +3,9 @@
 // This code is released under the GPL v3.0 License (SPDX-License-Identifier: AGPL-3.0)
 
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 extension EnkaDBGenerator {
     public static func compileEnkaDB(for game: SupportedGame, targeting outputURL: URL) async throws {
